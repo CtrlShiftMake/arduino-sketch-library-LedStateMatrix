6025cb2bd1fbd77684671ed30f6e0738b26733d0
Owen Moore <hello@owenmoore.art>
2021-01-26 00:46:39 -0800

    not able to read file on SD card

--- main.ino
+++ main.ino
@@ -1,10 +1,17 @@
-#include <RTClib.h>
-
 #include "src/LedMatrix/LedMatrix.h"
+
 #include <Arduino.h>
 #include <FastLED.h>
+#include <RTClib.h>
+#include <SD.h>
+#include <SPI.h>
 
 RTC_PCF8523 rtc;
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+File trackfile;
+const int chipSelect = 10;
 
 CRGB leds[LED_NUM];
 char daysOfTheWeek[7][12] = {"Sunday",   "Monday", "Tuesday", "Wednesday",
@@ -20,6 +27,8 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB
 #endif
 
+  // Initializing RTC for time
+  Serial.println("Initializing RTC...");
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
@@ -32,6 +41,43 @@ void setup() {
 
   rtc.start();
 
+  // Initializing SD card
+  /* Serial.println("Initializing SD card...");
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("Couldn't initialize SD card (missing or mis-configured");
+    Serial.flush();
+    abort();
+  }
+
+  if (!volume.init(card)) {
+    Serial.println("Couldn't find FAT16/FAT32 partition");
+    Serial.flush();
+    abort();
+  }
+
+  root.openRoot(volume);
+  */
+
+  // Initialize Debug Track File
+  pinMode(chipSelect, OUTPUT);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    Serial.flush();
+    abort();
+  }
+  Serial.println("card initialized.");
+  char filename[] = "test/test.json";
+  if (!SD.exists(filename)) {
+    trackfile = SD.open(filename, FILE_WRITE);
+  }
+  if (!trackfile) {
+    Serial.println("Couldn't created file");
+    Serial.flush();
+    abort();
+  }
+
+  // Initializing LED Matrix
+  Serial.println("Initializing LED Matrix");
   ledMatrix.init();
   ledMatrix.applyToCRGBArray(leds);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
