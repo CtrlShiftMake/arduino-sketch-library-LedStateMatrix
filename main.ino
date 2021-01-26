3060e0e4aaa1db321402325e8d518ab270fa3e6d
Owen Moore <hello@owenmoore.art>
2021-01-25 21:22:53 -0800

    moved debug testing from main loop

--- main.ino
+++ main.ino
@@ -1,6 +1,6 @@
+#include "src/LedMatrix/LedMatrix.h"
 #include <Arduino.h>
 #include <FastLED.h>
-#include "src/LedMatrix/LedMatrix.h"
 
 CRGB leds[LED_NUM];
 
@@ -14,50 +14,47 @@ void setup() {
   FastLED.show();
 }
 
-void loop() {
+void loop() {}
+
+void DebugLEDStates() {
+  delay(600);
+  for (int x = 0; x < LED_ARRAY_X; x++) {
+    for (int y = 0; y < LED_ARRAY_Y; y++) {
+      ledMatrix.setState(x, y, LED_DIMMED);
+    }
+  }
+  ledMatrix.applyToCRGBArray(leds);
+  Serial.println("DIMMED");
+  FastLED.show();
 
   delay(600);
-  for (int x = 0; x < LED_ARRAY_X; x++)
-  {
-      for (int y = 0; y < LED_ARRAY_Y; y++)
-      {
-          ledMatrix.setState(x, y, LED_DIMMED);
-      }
+  for (int x = 0; x < LED_ARRAY_X; x++) {
+    for (int y = 0; y < LED_ARRAY_Y; y++) {
+      ledMatrix.setState(x, y, LED_ACTIVE);
+    }
   }
+  ledMatrix.applyToCRGBArray(leds);
+  Serial.println("ACTIVE");
+  FastLED.show();
+
+  delay(600);
+  for (int x = 0; x < LED_ARRAY_X; x++) {
+    for (int y = 0; y < LED_ARRAY_Y; y++) {
+      ledMatrix.setState(x, y, LED_PRESSED);
+    }
+  }
+  Serial.println("PRESSED");
   ledMatrix.applyToCRGBArray(leds);
   FastLED.show();
 
   delay(600);
-  for (int x = 0; x < LED_ARRAY_X; x++)
-  {
-      for (int y = 0; y < LED_ARRAY_Y; y++)
-      {
-          ledMatrix.setState(x, y, LED_ACTIVE);
-      }
+  for (int x = 0; x < LED_ARRAY_X; x++) {
+    for (int y = 0; y < LED_ARRAY_Y; y++) {
+      ledMatrix.setState(x, y, LED_INACTIVE);
+    }
   }
   ledMatrix.applyToCRGBArray(leds);
-  FastLED.show();
-
-  delay(600);
-  for (int x = 0; x < LED_ARRAY_X; x++)
-  {
-      for (int y = 0; y < LED_ARRAY_Y; y++)
-      {
-          ledMatrix.setState(x, y, LED_PRESSED);
-      }
-  }
-  ledMatrix.applyToCRGBArray(leds);
-  FastLED.show();
-
-  delay(600);
-  for (int x = 0; x < LED_ARRAY_X; x++)
-  {
-      for (int y = 0; y < LED_ARRAY_Y; y++)
-      {
-          ledMatrix.setState(x, y, LED_INACTIVE);
-      }
-  }
-  ledMatrix.applyToCRGBArray(leds);
+  Serial.println("INACTIVE");
   FastLED.show();
 
   int r = random(0, 255);
@@ -65,5 +62,4 @@ void loop() {
   int b = random(0, 255);
 
   ledMatrix.setInputsColor(r, g, b);
-  
 }
